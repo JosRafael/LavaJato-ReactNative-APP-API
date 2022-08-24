665aaefe04976f1118f5b84bd7fb923a84e5f961
JRafael <rafaelmatiashue@gmail.com>
2022-08-24 13:08:50 -0300

    completed: pagina 1-Ok

--- APP/scr/style/StyledButtonInicio/index.js
+++ APP/scr/style/StyledButtonInicio/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { View, Text, Pressable } from "react-native";
-import styles from "../StyledButtonInicio/style";
+import styles from "./style";
 
 const StyledButton = (props) => {
   const { type, content, onPress } = props;
 
-  const backgroundColor = type === "primary" ? "#171A20CC" : "#FFFFFFA6";
-  const textColor = type === "primary" ? "#FFFFFF" : "#171A20";
+  const backgroundColor = type === "primary" ? "#171A20CC" : "#38a69e";
+  const textColor = type === "primary" ? "#FFFFFF" : "white";
 
   return (
     <View style={styles.container}>
       <Pressable
         style={[styles.button, { backgroundColor: backgroundColor }]}
-        onPress={() => onPress()}
+        onPress={onPress}
       >
         <Text style={[styles.text, { color: textColor }]}>{content}</Text>
       </Pressable>
--- APP/scr/telas/Pagina1.js
+++ APP/scr/telas/Pagina1.js
@@ -8,6 +8,7 @@ import {
   ImageBackground,
 } from "react-native";
 import styles from "../style/styles";
+import StyledButton from "../style/StyledButtonInicio";
 
 export default function Pagina1({ navigation }) {
   return (
@@ -20,6 +21,30 @@ export default function Pagina1({ navigation }) {
       <View style={styles.titles}>
         <Text style={styles.title}>Car Wash</Text>
         <Text style={styles.subtitle}>Version 1.0 aplicattion</Text>
+      </View>
+
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content={"Log In"}
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+        />
+        <StyledButton
+          type="secundary"
+          content={"Cadastrar-se"}
+          onPress={() => {
+            navigation.navigate("Cadastro");
+          }}
+        />
+        <StyledButton
+          type="primary"
+          content={"Modo Visitante"}
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        />
       </View>
     </View>
   );
